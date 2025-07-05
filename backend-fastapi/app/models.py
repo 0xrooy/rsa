b649ab3d7ff3f5dfd7e0f8b1000003ca588436e4
Rooy <nistelrooyreynold02@gmail.com>
2025-07-05 20:01:40 +0800

    Configure network between fastapi and next.js docker, changing frontend css style in globals.css and layout.css

--- backend-fastapi/app/models.py
+++ backend-fastapi/app/models.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Column, Integer, String
-from .database import Base
+from app.database import Base
 
 class Gamer(Base):
     __tablename__ = "gamer"
-
     userID = Column(Integer, primary_key=True, index=True)
     username = Column(String(100), nullable=False)
     score = Column(Integer, nullable=False)
